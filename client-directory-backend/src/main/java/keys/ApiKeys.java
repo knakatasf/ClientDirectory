adfdcfe2c2acb63f5f8ffc93551f983f3a8cf393
Claudioflew <126118326+Claudioflew@users.noreply.github.com>
2024-09-09 16:17:43 -0700

    Added gitignore

--- client-directory-backend/src/main/java/keys/ApiKeys.java
+++ client-directory-backend/src/main/java/keys/ApiKeys.java
@@ -2,6 +2,6 @@ package keys;
 
 public class ApiKeys {
     // Add your OpenAI Key before running examples
-    public final static String API_KEY_OPEN_AI = "sk-H9Aqrt5dptfgBvclPlyUChuxLi6FUmrCPdN52z6G7ZT3BlbkFJ7uASFcMUCUt9qvIqCg06dLhJNQNZiQ_iqaima1XWYA";
+    public final static String API_KEY_OPEN_AI = "";
     //public final static String API_ANTHROPIC = "";
 }
